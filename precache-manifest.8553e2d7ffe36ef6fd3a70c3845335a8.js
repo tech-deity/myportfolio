90b97399d973c7bad4bf88f3e01e36a2805b9fdd
tech-deity <1316510155@kit.ac.in>
2019-12-16 20:13:33 +0530

    Updates

---
+++ precache-manifest.8553e2d7ffe36ef6fd3a70c3845335a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef20a0f2473d0a3077047c02d251bb0",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "d48af1198dc54a27f925",
+    "url": "/myportfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a932451d177a4cb385e5",
+    "url": "/myportfolio/static/js/2.47066050.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/myportfolio/static/js/2.47066050.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d48af1198dc54a27f925",
+    "url": "/myportfolio/static/js/main.300d2dff.chunk.js"
+  },
+  {
+    "revision": "39123648c5215ea29ecf",
+    "url": "/myportfolio/static/js/runtime-main.e2511377.js"
+  },
+  {
+    "revision": "ed8f0e36bd625bc1ee3fadd5b0b98e36",
+    "url": "/myportfolio/static/media/logo.ed8f0e36.svg"
+  }
+]);
